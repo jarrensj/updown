9983e4400d78a877e980a25f5c6c919536bbb0aa
Jarren San Jose <sanjose.jarren@gmail.com>
2018-03-03 16:09:38 -0800

    init and route login and register component

--- updown/src/app/app-routing.module.ts
+++ updown/src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HappysadComponent } from './components/happysad/happysad.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  {path: 'happysad', component: HappysadComponent}
+  {path: 'happysad', component: HappysadComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+
 ];
 
 @NgModule({
--- updown/src/app/app.module.ts
+++ updown/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { HappysadComponent } from './components/happysad/happysad.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LogoComponent,
-    HappysadComponent
+    HappysadComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
