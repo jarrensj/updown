5abfac4835be991eb7d94940972be92ed555474b
Jarren San Jose <sanjose.jarren@gmail.com>
2018-05-28 20:57:29 -0700

    get last 8 months of dates

--- updown/src/app/components/profile/profile.component.ts
+++ updown/src/app/components/profile/profile.component.ts
@@ -12,6 +12,7 @@ export class ProfileComponent implements OnInit {
   user:string = "";
   firstName:string;
   log:Log[];
+  dates:any;
 
   constructor(private dataService:DataService,  public authService: AuthService) { }
 
@@ -22,7 +23,49 @@ export class ProfileComponent implements OnInit {
     this.dataService.getProfile(this.user, token).subscribe((user)=> {
       this.firstName = user[0].firstName;
       this.log = user[0].log;
-    })
+    });
+    this.dates = [];
+    this.calendar();
+  }
+
+  calendar() {
+    var days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    // get date
+    var today = new Date();
+    // get last eight months
+    var month = today.getMonth() + 1;
+    var year = today.getFullYear();
+    var last8months = [];
+    var theYears = [];
+    var temp;
+    var prevYear = false;
+    for (var i = 1; i < 9; ++i) {
+      temp = month - i;
+      if(temp < 0) {
+        temp = temp + 12;
+        theYears.push(year - 1);
+        prevYear = true;
+      }
+      else {
+        prevYear = false;
+      }
+      if(temp >= 0) {
+        last8months.push(temp);
+        if(!prevYear) {
+          theYears.push(year);
+        }
+      }
+    }
+    // make an array of all the dates in the last eight months
+    // last eight months
+    for (var i = 0; i < 8; ++i) {
+      // go through the amount of days in each of those months
+      for (var j = days_in_month[last8months[i]]; j > 1; --j) {
+        temp = (last8months[i] + 1)+ '/' + j + '/' + theYears[i];
+        this.dates.push(new Date(temp));
+      }
+    }
+    console.log(this.dates);
   }
 
 }
